4d6ea0231fbe4b533e419156c9d0679d552e01aa
Abhiyansh Vaishnav <89480538+abhiyanshh27@users.noreply.github.com>
2022-10-02 05:06:47 +0530

    Add files via upload

---
+++ Tel1.py
@@ -0,0 +1,15 @@
+class Computer:
+    def config(self):
+        print("i5, 16gb, 1TB")
+
+Com1 = Computer()
+print(type(Com1))
+
+'''
+i = 9
+f = 9.75
+s = 'Aman'
+print(type(i))
+print(type(f))
+print(type(s))
+'''
